1afc00365de42b5b311fcdd59fa869ef56672cee
Ryan Webb <65420906+Ryan-Webb62@users.noreply.github.com>
2020-12-02 10:28:59 -0500

    add lecture on functions

--- script.js
+++ script.js
@@ -1,9 +1,30 @@
 'use strict';
-
+/* 
 let hasDriversLicense = false;
 const passTest = true;
 
 if (passTest) hasDriversLicense = true;
 if (hasDriversLicense) console.log('I can drive :D');
 
-const private = 534;
+const private = 534; */
+
+// Functions
+/* 
+function logger() {
+  console.log('My name is Jonas');
+}
+
+// calling/ invoking/ running the function:
+logger();
+logger();
+logger();
+ */
+function foodProccesor(apples, oranges) {
+  console.log(apples, oranges);
+  const juice = `Juice with ${apples} apples and ${oranges} oranges.`;
+  return juice;
+}
+
+const appleJuice = foodProccesor(5, 0);
+console.log(appleJuice);
+console.log(foodProccesor(5, 0));
