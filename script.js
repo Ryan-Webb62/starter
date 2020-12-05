5fabb0e430d9265c1089079a90b4c46329f53024
Ryan Webb <65420906+Ryan-Webb62@users.noreply.github.com>
2020-12-05 12:04:13 -0500

    add lecture function declaration and expressions

--- script.js
+++ script.js
@@ -19,6 +19,8 @@ logger();
 logger();
 logger();
  */
+
+/* 
 function foodProccesor(apples, oranges) {
   console.log(apples, oranges);
   const juice = `Juice with ${apples} apples and ${oranges} oranges.`;
@@ -28,3 +30,23 @@ function foodProccesor(apples, oranges) {
 const appleJuice = foodProccesor(5, 0);
 console.log(appleJuice);
 console.log(foodProccesor(5, 0));
+ */
+
+// Function Declarations vs. Expressions
+
+// Function declaration - declared functions can be called before they are declared
+
+function calcAge1(bithYear) {
+  return 2037 - bithYear;
+}
+
+const age1 = calcAge1(1991);
+
+// Function expression - must be called after being declared
+const calcAge2 = function (bithYear) {
+  return 2037 - bithYear;
+};
+
+const age2 = calcAge2(1991);
+
+console.log(age1, age2);
