49dcb67dbde14b21cca21ee78cf9e3dd3be4187e
Ryan Webb <65420906+Ryan-Webb62@users.noreply.github.com>
2020-12-09 20:57:58 -0500

    Add function review lecture

--- script.js
+++ script.js
@@ -69,7 +69,7 @@ console.log(yearsUntilRetirement(1991, 'Jonas'));
 console.log(yearsUntilRetirement(1980, 'Bob')); */
 
 // Functions calling other functions
-
+/* 
 function cutFruitPieces(fruit) {
   return fruit * 4;
 }
@@ -83,3 +83,27 @@ function fruitProcessor(apples, oranges) {
 }
 
 console.log(fruitProcessor(2, 3));
+ */
+
+// Reviewing Functions
+
+const calcAge = function (birthYear) {
+  return 2037 - birthYear;
+};
+
+const yearsUntilRetirement = function (birthYear, firstName) {
+  const age = calcAge(birthYear);
+  const retirement = 65 - age;
+  // return `${firstName} retires in ${retirement} years.`;
+
+  if (retirement > 0) {
+    console.log(`${firstName} retires in ${retirement} years.`);
+    return retirement;
+  } else {
+    console.log(`${firstName} has already retired `);
+    return -1;
+  }
+};
+
+console.log(yearsUntilRetirement(1991, 'Jonas'));
+console.log(yearsUntilRetirement(1950, 'Mike'));
