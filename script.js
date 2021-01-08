4c7638f3800ec0543219711b5e929966baf9fcf4
Ryan <65420906+Ryan-Webb62@users.noreply.github.com>
2021-01-08 18:13:49 -0500

    Add array filter method lecture

--- script.js
+++ script.js
@@ -351,7 +351,7 @@ for (let exercise = 1; exercise < 4; exercise++) {
  */
 
 // While Loops
-
+/* 
 for (let rep = 0; rep <= 10; rep++) {
   console.log(`Lifting weights repetition ${rep}`);
 }
@@ -371,3 +371,63 @@ while (dice != 6) {
   dice = Math.trunc(Math.random() * 6) + 1;
   if (dice === 6) console.log(`Loop is about to end...`);
 }
+ */
+
+// Array Methods
+const characters = [
+  {
+    name: 'Luke Skywalker',
+    height: '172',
+    mass: '77',
+    eye_color: 'blue',
+    gender: 'male',
+  },
+  {
+    name: 'Darth Vader',
+    height: '202',
+    mass: '136',
+    eye_color: 'yellow',
+    gender: 'male',
+  },
+  {
+    name: 'Leia Organa',
+    height: '150',
+    mass: '49',
+    eye_color: 'brown',
+    gender: 'female',
+  },
+  {
+    name: 'Anakin Skywalker',
+    height: '188',
+    mass: '84',
+    eye_color: 'blue',
+    gender: 'male',
+  },
+];
+
+// ***FILTER***
+//1. Get characters with mass greater than 100
+const greater100Characters = characters.filter((character) => {
+  return character.mass > 100;
+});
+console.log(greater100Characters);
+// ES6 arrow function allows for this also:
+// const greater100Characters = characters.filter(
+//   (characters) => character.mass > 100
+// );
+
+//2. Get characters with height less than 200
+const shorterCharacters = characters.filter(
+  (character) => character.height < 200
+);
+console.log(shorterCharacters);
+//3. Get the male characters
+const maleCharacters = characters.filter(
+  (character) => character.gender === 'male'
+);
+console.log(maleCharacters);
+//4. Get the female characters
+const femaleCharacters = characters.filter(
+  (character) => character.gender === 'female'
+);
+console.log(femaleCharacters);
